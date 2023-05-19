962889b0faeff5f31888a88429dbcbb6d834c900
Jorge Quintui <jorgealequinn@gmail.com>
2023-05-19 00:46:26 -0400

    UPDATE: Dataframe

--- dataframe.py
+++ dataframe.py
@@ -1,16 +1,13 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn import linear_model
-from sklearn.metrics import r2_score
 
 path = './aircraft_db_2008.csv'
 data = pd.read_csv(path)
 
 df = data.dropna(subset=['ArrDelay'])
-df = df.sample(frac=1).head(1000)
+df = df.sample(frac=1).head(5000)
+
+df = df.reset_index()
 
 X = df[['AirTime','Distance','DepDelay']]
 Y = df['ArrDelay']
-
-
 
